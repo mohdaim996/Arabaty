fee20defd8566adf17e0a1d4d8699ad5d23689d1
mohdaim996 <moh.daim996@gmail.com>
2021-06-10 13:08:30 +0300

    qr code

--- lib/qrcode.dart
+++ lib/qrcode.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
-import '../main.dart';
-
 class QRScanPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _QRScanPageState();
@@ -13,58 +11,78 @@ class _QRScanPageState extends State<QRScanPage> {
   String qrCode = 'Unknown';
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Scan Result',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white54,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '$qrCode',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 72),
+            ButtonWidget(
+              text: 'Start QR scan',
+              onClicked: () => scanQRCode(),
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Scan Result',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white54,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                '$qrCode',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(height: 72),
-              ButtonWidget(
-                              text: 'Start QR scan',
-                              onClicked: () => scanQRCode(),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-              
-                Future<void> scanQRCode() async {
-                  try {
-                    final qrCode = await FlutterBarcodeScanner.scanBarcode(
-                      '#ff6666',
-                      'Cancel',
-                      true,
-                      ScanMode.QR,
-                    );
-              
-                    if (!mounted) return;
-              
-                    setState(() {
-                      this.qrCode = qrCode;
-                    });
-                  } on PlatformException {
-                    qrCode = 'Failed to get platform version.';
-                  }
-                }
-              
-                ButtonWidget({String? text, Future<void> Function()? onClicked}) {}
+      ),
+    );
+  }
+
+  Future<void> scanQRCode() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.QR,
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.qrCode = qrCode;
+      });
+    } on PlatformException {
+      qrCode = 'Failed to get platform version.';
+    }
+  }
+}
+
+class ButtonWidget extends StatelessWidget {
+  String text = '';
+  VoidCallback onClicked;
+
+  ButtonWidget({
+    required this.text,
+    required this.onClicked,
+  });
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 24),
+        ),
+       
+  
+        onPressed: onClicked,
+      );
 }
